804b839fc50196f07a0fb4675914dd6c4a1a582a
IT24103496 <IT24103496@my.sliit.lk>
2025-08-21 16:56:06 +0530

    Add files via upload

---
+++ Lab Sheet 04/IT24103496.R
@@ -0,0 +1,27 @@
+setwd("C:/Users/it24103496/Desktop/it24103496")
+
+branch_data <- read.csv("Exercise.txt",header = TRUE)
+
+head(branch_data)
+
+str(branch_data)
+
+boxplot(branch_data$Sales_X1 , main ="Boxplot of Sales", ylab = "sales")
+
+fivenum(branch_data$Advertising_X2)
+
+IQR(branch_data$Advertising_X2)
+
+find_outliers <-function(x) {
+  Q1 <- quantile(x , 0.25)
+  Q3 <- quantile(X , 0.75)
+  IQR_value <- Q3 - Q1
+  lower_bound <- Q1 - 1.5 * IQR_value
+  upper_bound <- Q3 + 1.5 *IQR_value
+  outliers <- x[x < lower_bound | x > upper_bound]
+  return(outliers)
+  
+}
+find_outliers(branch_data$Years_x3)
+}
+
